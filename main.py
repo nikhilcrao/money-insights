164c7e050aad90a2ca4c9dbe639b6e2acd8171b7
Nikhil Rao <nikhilcrao@gmail.com>
2022-10-16 20:26:39 +0530

    add initial commits for the category

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 import datetime
 import google.oauth2.id_token
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from google.auth.transport import requests
 from google.cloud import datastore
 
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import DataRequired
+
 app = Flask(__name__)
+app.config.update({
+    'SECRET_KEY': 'test',
+})
 
 datastore_client = datastore.Client()
 
@@ -27,6 +34,21 @@ def fetch_times(email, limit):
     return times
 
 
+def store_category(email, name):
+    entity = datastore.Entity(key=datastore_client.key('User', email, 'category'))
+    entity.update({
+        'name': name,
+    })
+    datastore_client.put(entity)
+
+
+def fetch_categories(email, limit=None):
+    ancestor = datastore_client.Key('User', email)
+    query = datastore_client.query(kind='category', ancestor=ancestor)
+    categories = query.fetch(limit=limit)
+    return categories
+
+
 @app.route('/')
 def root():
     id_token = request.cookies.get('token')
@@ -43,6 +65,59 @@ def root():
             error_message = str(err)
 
     return render_template('index.html', user_data=claims, error_message=error_message, times=times)
+
+
+@app.route('/category/', methods=['GET'])
+def category_home():
+    id_token = request.cookies.get('token')
+    error_message = None
+    claims = None
+    categories = None
+
+    if id_token:
+        try:
+            claims = google.oauth2.id_token.verify_firebase_token(id_token, firebase_request_adapter)
+            categories = fetch_categories(claims['email'])
+        except ValueError as err:
+            error_message = str(err)
+
+    return render_template('category_index.html', user_data=claims, error_message=error_message, categories=categories)
+
+
+
+class AddCategoryForm(FlaskForm):
+    name = StringField('name', validators=[DataRequired()])
+
+
+@app.route('/category/add', methods=['GET', 'POST'])
+def add_category():
+    form = AddCategoryForm()
+    if form.validate_on_submit():
+        id_token = request.cookies.get('token')
+        error_message = None
+        claims = None
+        categories = None
+
+        if id_token:
+            try:
+                claims = google.oauth2.id_token.verify_firebase_token(id_token, firebase_request_adapter)
+                store_category(claims['email'], form.name)
+                return redirect('/category/')
+            except ValueError as err:
+                error_message = str(err)
+
+
+    return render_template('category_add.html', form=form)
+
+
+@app.route('/category/edit/<category_slug>')
+def edit_category(category_slug):
+    pass
+
+
+@app.route('/category/remove/<category_slug>')
+def remove_category(category_slug):
+    pass
 
 
 if __name__ == '__main__':
